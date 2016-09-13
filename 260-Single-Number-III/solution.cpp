70490792e17e336ff89de08e204572f55e3cf0c8
haomingchan0811 <haomingchan0811@users.noreply.github.com>
2016-09-13 12:47:30 -0400

    [260. Single Number III][Accepted]committed by LeetCode Extension

--- 260-Single-Number-III/solution.cpp
+++ 260-Single-Number-III/solution.cpp
@@ -1,9 +1,32 @@
 class Solution {
 public:
+    // linear complexity: bit manipulation
+    vector<int> singleNumber(vector<int>& nums){
+        int xorXY = 0;
+        // exclusive or all items, result will be x xor y (numbers only showed up once)
+        for(int i = 0; i < nums.size(); i ++)
+            xorXY ^= nums[i];
+        // finding the LSB (least significant bit): x & (-x)
+        int lsb = xorXY & (-xorXY);
+        int X = 0, Y = 0;
+        for(int i = 0; i < nums.size(); i ++){
+            if(lsb & nums[i])
+                X ^= nums[i];
+            else
+                Y ^= nums[i];
+        }
+        vector<int> ans;
+        ans.push_back(X);
+        ans.push_back(Y);
+        return ans;
+    }
+
+    // pointer: 105ms
+    /*
     vector<int> singleNumber(vector<int>& nums) {
         sort(nums.begin(), nums.end());
         vector<int> ans;
-        int inter = nums[0];    // pointer 
+        int inter = nums[0];    // pointer
         int count = 1;
         int ansCount = 0;
         for(int i = 1; (i < nums.size()) && (ansCount != 2); i ++){
@@ -13,16 +36,14 @@ public:
                 ansCount += 1;
                 inter = nums[i];
                 count = 1;
-                cout << i << " " << count << " " << nums[i] << endl;
             }
             else{
                 i += 1;
                 count += 1;
                 inter = nums[i];
-                cout << "-" << i << " " << count << " " << nums[i] << endl;
             }
         }
         if(ansCount == 1) ans.push_back(inter);
         return ans;
-    }
+    }*/
 };
