8956825486ed2e0a4ffb86b7df26ffff3438adae
haomingc <haomingchan0811@users.noreply.github.com>
2016-09-19 01:26:35 -0400

    [268. Missing Number][Accepted]committed by LeetCode Extension

--- 268-Missing-Number/solution.cpp
+++ 268-Missing-Number/solution.cpp
@@ -1,10 +1,25 @@
 class Solution {
 public:
+    // math solution: 29ms
+    /*
     int missingNumber(vector<int>& nums) {
         int sum = 0;
         for(auto temp: nums)
             sum += temp;
         int trueSum = nums.size() * (nums.size() + 1) / 2;
         return trueSum - sum;
+    }*/
+
+    // bit manipulation - exclusive or:
+    int missingNumber(vector<int>& nums) {
+        int ans = 0;
+        int cur = 0;
+        for(auto n: nums){
+            ans ^= n;
+            ans ^= cur;
+            cur ++;
+        }
+        ans ^= cur;
+        return ans;
     }
 };
