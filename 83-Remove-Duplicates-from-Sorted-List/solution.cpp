ce8a3bb35f494f51378a05b4e49da2d9738836d6
haomingc <haomingchan0811@users.noreply.github.com>
2016-09-22 00:01:53 -0400

    [83. Remove Duplicates from Sorted List][Runtime Error]committed by LeetCode Extension

---
+++ 83-Remove-Duplicates-from-Sorted-List/solution.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode* cur = head;
+        ListNode* nextNode = cur->next;
+        while(nextNode != NULL){
+            while(nextNode->val == cur->val){
+                nextNode = nextNode->next;
+            }
+            cur->next = nextNode;
+            cur = nextNode;
+            nextNode = cur->next;
+        }
+        return head;
+    }
+};
