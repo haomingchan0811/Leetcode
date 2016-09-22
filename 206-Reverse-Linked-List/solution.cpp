f9a3962b2e90095779b9eb3fd89ad0cfc19999a8
haomingc <haomingchan0811@users.noreply.github.com>
2016-09-21 22:48:54 -0400

    [206. Reverse Linked List][Compile Error]committed by LeetCode Extension

---
+++ 206-Reverse-Linked-List/solution.cpp
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* cur = head;
+        ListNode* prev = NULL;
+        while(cur != NULL){
+            ListNode* nextNode = cur->next;
+            cur->next = prev;
+            prev = cur;
+            cur = nextNode;
+        }
+    }
+};
