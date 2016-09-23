c8e11a70d4b2efbb4d4eeb4fafb006c2d1d2b04a
haomingc <haomingchan0811@users.noreply.github.com>
2016-09-22 23:36:59 -0400

    [238. Product of Array Except Self][Accepted]committed by LeetCode Extension

--- 238-Product-of-Array-Except-Self/solution.cpp
+++ 238-Product-of-Array-Except-Self/solution.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    // Brute-force: O(n^2)
+    // Brute-force: O(n^2) - Time Limit Exceed
+    /*
     vector<int> productExceptSelf(vector<int>& nums) {
         vector<int> ret;
         int len = nums.size();
@@ -16,6 +17,20 @@ public:
         for(int i = 0; i < len; i ++)
             ret.push_back(pro[i]);
         delete [] pro;
+        return ret;
+    }*/
+    
+    // Two way multiply
+    vector<int> productExceptSelf(vector<int>& nums){
+        int len = nums.size();
+        vector<int> ret(len, 1);    // bug: ret[] can't be accessed when ret hasn't initialized
+        for(int i = 1; i < len; i ++)
+            ret[i] = nums[i - 1] * ret[i - 1];
+        int p = nums[len - 1];
+        for(int i = len - 2; i >= 0; i --){
+            ret[i] *= p;
+            p *= nums[i];
+        }
         return ret;
     }
 };
