f74bb09f42ca877ccd7940b06411dc1ecddba7c9
haomingc <haomingchan0811@users.noreply.github.com>
2016-09-24 02:04:41 -0400

    [100. Same Tree][Accepted]committed by LeetCode Extension

--- 100-Same-Tree/solution.cpp
+++ 100-Same-Tree/solution.cpp
@@ -11,7 +11,7 @@ class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
         if(p == NULL && q == NULL) return true;
-        else if(p == NULL || q == NULL) return false;
+        else if(p == NULL || q == NULL) return false; // bug: NULL judgement must come first
         else return (p->val == q->val) && isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
     }
 };
