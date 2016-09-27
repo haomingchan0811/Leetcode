2a168e38abdb2175b55369b92fa48dad4a6361d0
haomingc <haomingchan0811@users.noreply.github.com>
2016-09-26 22:15:45 -0400

    [104. Maximum Depth of Binary Tree][Compile Error]committed by LeetCode Extension

--- 104-Maximum-Depth-of-Binary-Tree/solution.cpp
+++ 104-Maximum-Depth-of-Binary-Tree/solution.cpp
@@ -9,8 +9,28 @@
  */
 class Solution {
 public:
+    // DFS: 6ms
+    /*
     int maxDepth(TreeNode* root) {
         if(root == NULL) return 0;
         else return max(maxDepth(root->left), maxDepth(root->right)) + 1;
+    }*/
+    
+    // BFS: 
+    int maxDepth(TreeNode* root){
+        if(root == NULL) return 0;
+        queue<int> que;
+        que.push(root);
+        int depth = 0;
+        while(!que.empty()){
+            depth ++;       // every layer of nodes
+            for(int i = 0; i < que.size(); i ++){
+                Tree* temp = que.front();
+                que.pop();
+                if(temp->left != NULL) que.push(temp->left);
+                if(temp->right != NULL) que.push(temp->right);
+            }
+        }
+        return depth;
     }
 };
