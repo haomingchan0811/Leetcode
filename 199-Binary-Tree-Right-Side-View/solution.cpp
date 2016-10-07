0ceee86bffe2bbe41fcaa246ab9be2d3c12bb9dc
haomingc <haomingchan0811@users.noreply.github.com>
2016-10-06 23:51:53 -0400

    [199. Binary Tree Right Side View][Accepted]committed by LeetCode Extension

---
+++ 199-Binary-Tree-Right-Side-View/solution.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> ret;
+        if(root == NULL) return ret;
+        queue<TreeNode*> que;
+        que.push(root);
+        while(true){
+            int queueSize = que.size();  // number of nodes for the current level of tree
+            if(queueSize == 0) break;   
+            int rightMost;              // the value of the right most tree node
+            while(queueSize > 0){       // traverse a level of the tree
+                TreeNode* temp = que.front();
+                que.pop();
+                rightMost = temp->val;
+                if(temp->left != NULL) que.push(temp->left);
+                if(temp->right != NULL) que.push(temp->right);
+                queueSize --;
+            }
+            ret.push_back(rightMost);
+        }
+        return ret;
+    }
+};
