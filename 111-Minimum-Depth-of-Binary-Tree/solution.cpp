48243ca83a8eaf3fb02d53bc7d49088558d7699e
haomingc <haomingchan0811@users.noreply.github.com>
2016-10-10 14:13:02 -0400

    [111. Minimum Depth of Binary Tree][Accepted]committed by LeetCode Extension

--- 111-Minimum-Depth-of-Binary-Tree/solution.cpp
+++ 111-Minimum-Depth-of-Binary-Tree/solution.cpp
@@ -10,32 +10,39 @@
 class Solution {
 public:
     // DFS: 
-    // int minDepth(TreeNode* root){
-    //     if(!root) return 0;
-    //     int minDepth =
-    // }
-
-
-    // BFS: 16ms
-    int minDepth(TreeNode* root) {
-        if(root == NULL) return 0;
-        // int minDepth = INT_MAX;
-        queue<TreeNode*> que;
-        que.push(root);
-        int level = 0;
-        while(!que.empty()){
-            int size = que.size();
-            level++;
-            while(size--){
-                TreeNode* temp = que.front();
-                que.pop();
-                // if(!temp->left && !temp->right && minDepth > level) minDepth = level;
-                if(!temp->left && !temp->right) return level;
-                if(temp->left) que.push(temp->left);
-                if(temp->right) que.push(temp->right);
-            }
-        }
-        // return minDepth;
-        return 0;
+    int minDepth(TreeNode* root){
+        if(!root) return 0;
+        int level = 1;
+        return DFS(root, INT_MAX, level);
     }
+    
+    int DFS(TreeNode* root, int minDepth, int level){
+        if(!root) return INT_MAX;
+        if(!root->left && !root->right && minDepth > level) return level;
+        return min(DFS(root->left, minDepth, level + 1), DFS(root->right, minDepth, level + 1));
+    }
+
+
+    // BFS: 16ms -> 9ms(replace the anotated lines with their below lines, stop when found the first leaf)
+    // int minDepth(TreeNode* root) {
+    //     if(root == NULL) return 0;
+    //     // int minDepth = INT_MAX;
+    //     queue<TreeNode*> que;
+    //     que.push(root);
+    //     int level = 0;
+    //     while(!que.empty()){
+    //         int size = que.size();
+    //         level++;
+    //         while(size--){
+    //             TreeNode* temp = que.front();
+    //             que.pop();
+    //             // if(!temp->left && !temp->right && minDepth > level) minDepth = level;
+    //             if(!temp->left && !temp->right) return level;
+    //             if(temp->left) que.push(temp->left);
+    //             if(temp->right) que.push(temp->right);
+    //         }
+    //     }
+    //     // return minDepth;
+    //     return 0;
+    // }
 };
