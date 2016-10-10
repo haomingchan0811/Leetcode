85bd3f3332498f38eb83f6efc041278975633c28
haomingc <haomingchan0811@users.noreply.github.com>
2016-10-10 01:42:36 -0400

    [102. Binary Tree Level Order Traversal][Accepted]committed by LeetCode Extension

--- 102-Binary-Tree-Level-Order-Traversal/solution.cpp
+++ 102-Binary-Tree-Level-Order-Traversal/solution.cpp
@@ -11,6 +11,7 @@ class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
         vector<vector<int>> traversal;
+        if(root == NULL) return traversal;
         queue<TreeNode*> que;
         que.push(root);
         while(!que.empty()){
