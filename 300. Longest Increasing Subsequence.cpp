1d023924a177fbe65c1c9c6b34e7d4cabb7bd971
Haoming Chen <chenhaomingsz@gmail.com>
2017-02-26 23:39:53 -0500

    2017.2.26

--- 300. Longest Increasing Subsequence.cpp
+++ 300. Longest Increasing Subsequence.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    // dp[i]: length of LIS ending at j
+    // dp[i]: length of LIS ending at i
     // O(N2)time, O(N)space, 57.19%, ok
     int lengthOfLIS(vector<int>& nums) {
         if(nums.empty()) return 0;
@@ -12,6 +12,32 @@ public:
                     len[i] = max(len[i], len[j] + 1);
             }
             ret = max(ret, len[i]);
+        }
+        return ret;
+    }
+
+    // binary search: O(NlogN)time, O(N)space, 75.86%, ok
+    // keep an increasing list, replace the smallest elem that is larger than nums[i]
+    int lengthOfLIS(vector<int>& nums) {
+        if(nums.empty()) return 0;
+        int ret = 1; // base
+        vector<int> list(nums.size(), INT_MAX); // initialize to max
+        list[0] = nums[0];
+        for(int i = 1; i < nums.size(); i++){
+            // binary search: find smallest elem in the list which > nums[i]
+            int head = 0, tail = i;
+            while(head < tail){
+                int mid = head + (tail - head) / 2;
+                // bug: if(list[mid] <= nums[i])
+                if(list[mid] < nums[i]) head = mid + 1;
+                else if(list[mid] == nums[i]){
+                    head = mid;
+                    break;
+                }
+                else tail = mid;
+            }
+            list[head] = nums[i];
+            ret = max(ret, head + 1);
         }
         return ret;
     }
