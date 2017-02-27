61c5b2a60afb6f5243c826e595c96cba354a6ebf
Haoming Chen <chenhaomingsz@gmail.com>
2017-02-27 12:54:09 -0500

    2017.2.27

--- 64. Minimum Path Sum.cpp
+++ 64. Minimum Path Sum.cpp
@@ -1,22 +1,17 @@
-#include <iostream>
-#include <vector>
-using namespace std;
-
 class Solution {
 public:
-    // dynamic programming: 12ms
+    // dp[i][j]: current minimum sum
+    // O(mn)time, no space, 8.08%, ok
     int minPathSum(vector<vector<int>>& grid) {
-        int row = grid.size();
-        int col = grid[0].size();
-        int matrix[row][col];
-        matrix[0][0] = grid[0][0];
-        for(int i = 1; i < row; i++) matrix[i][0] = matrix[i - 1][0] + grid[i][0]; // initialize the first column
-        for(int j = 1; j < col; j++) matrix[0][j] = matrix[0][j - 1] + grid[0][j]; // initialize the first row
-        for(int i = 1; i < row; i++){
-            for(int j = 1; j < col; j++){
-                matrix[i][j] = min(matrix[i - 1][j], matrix[i][j - 1]) + grid[i][j];
+        if(grid.empty()) return 0;
+        int m = grid.size(), n = grid[0].size();
+        for(int i = 1; i < m; i++) grid[i][0] += grid[i - 1][0];
+        for(int i = 1; i < n; i++) grid[0][i] += grid[0][i - 1];
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j < n; j++){
+                grid[i][j] += min(grid[i - 1][j], grid[i][j - 1]);
             }
         }
-        return matrix[row - 1][col - 1];
+        return grid[m - 1][n - 1];
     }
 };
