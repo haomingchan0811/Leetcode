4dca5f6bcf1abc2c1402082916c608077be36d4d
Haoming Chen <chenhaomingsz@gmail.com>
2017-03-03 13:13:42 -0500

    2017.3.3

--- 81. Search in Rotated Sorted Array II.cpp
+++ 81. Search in Rotated Sorted Array II.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
-    // binary search: 6ms worst O(n), avg: O(logN)
+    // binary search with duplicates: O(logN)time (worst O(N)), O(1)space, 35.84%, ok
     bool search(vector<int>& nums, int target) {
-        int low = 0;
-        int high = nums.size() - 1;
-        while(low <= high){
-            int mid = low + (high - low) / 2;
+        if(nums.empty()) return false;
+        int head = 0, tail = nums.size() - 1;
+        while(head <= tail){
+            int mid = head + (tail - head) / 2;
             if(nums[mid] == target) return true;
-            if(nums[mid] > nums[low]){
-                if(nums[low] <= target && target < nums[mid]) high = mid;
-                else low = mid + 1;
+            if(nums[mid] > nums[head]){   // monotone
+                if(nums[head] <= target && target < nums[mid])
+                    tail = mid - 1;
+                else head = mid + 1;
             }
-            // different from 33.Search in Rotated Sorted Array I
-            else if(nums[mid] == nums[low]){
-                low += 1;
+            else if(nums[mid] == nums[head]){   // curcial!!! no longer monotone
+                head++;
             }
             else{
-                if(nums[mid] < target && target <= nums[high]) low = mid + 1;
-                else high = mid;
+                if(nums[mid] < target && target <= nums[tail])
+                    head = mid + 1;
+                else tail = mid - 1;
             }
         }
         return false;
