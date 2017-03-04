f5c3bf2f4a5a89104591689e2b0f5f19b6dbf4f1
Haoming Chen <chenhaomingsz@gmail.com>
2017-03-03 20:32:18 -0500

    2017.3.3

--- 26. Remove Duplicates from Sorted Array .cpp
+++ 26. Remove Duplicates from Sorted Array .cpp
@@ -1,39 +1,26 @@
 class Solution {
 public:
-    // two pointers: 45ms
+    // two pointers: O(N)time, O(1)space, 52.71%, ok
     int removeDuplicates(vector<int>& nums) {
-        if(nums.size() <= 1) return nums.size();
-        int index = 0;
+        if(nums.empty()) return 0;
+        int curr = 0;  // pointer of the last distinct value
         for(int i = 1; i < nums.size(); i++){
-            if(nums[index] != nums[i]){
-                index++;
-                nums[index] = nums[i];
+            if(nums[i] != nums[curr]){
+                swap(nums[++curr], nums[i]);
             }
         }
-        return index + 1;
+        return curr + 1;
     }
 
-    // two pointers: 46ms
-    int removeDuplicates(vector<int>& nums){
-        if(nums.size() <= 1) return nums.size();
-        int cnt = 0;
-        for(int i = 1; i < nums.size(); i++){
-            if(nums[i] == nums[i - 1]) cnt++;
-            else nums[i - cnt] = nums[i];
-        }
-        return nums.size() - cnt;
-    }
-
-    // two pointers: 23ms
+    // two pointers without swap: O(N)time, O(1)space, 52.71%, ok
     int removeDuplicates(vector<int>& nums) {
-        if(nums.size() <= 1) return nums.size();
-        int index = 1;
+        if(nums.size() < 2) return nums.size();
+        int replace = 1;  // pointer of the next replaced position
         for(int i = 1; i < nums.size(); i++){
-            if(nums[index - 1] != nums[i]){
-                nums[index] = nums[i];
-                index++;
+            if(nums[i] != nums[replace - 1]){
+                nums[replace++] = nums[i];
             }
         }
-        return index;
+        return replace;
     }
 };
