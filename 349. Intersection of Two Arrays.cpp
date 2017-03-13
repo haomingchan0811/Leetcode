b3ee06051874957db7d830be74528dfd37f4a5e9
Haoming Chen <chenhaomingsz@gmail.com>
2017-03-13 01:44:44 -0400

    2017.3.13

--- 349. Intersection of Two Arrays.cpp
+++ 349. Intersection of Two Arrays.cpp
@@ -1,57 +1,35 @@
-#include <iostream>
-#include <vector>
-#include <unordered_map>
-#include <unordered_set>
-using namespace std;
+class Solution {
+public:
+    // two pointers: O(max(mlogm, nlogn))time, O(1)space, 58.27%, ok
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> ret;
+        if(nums1.empty() || nums2.empty()) return ret;
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+        for(int i = 0, j = 0; i < nums1.size() && j < nums2.size();){
+            if(nums1[i] == nums2[j]){
+                ret.push_back(nums1[i]);
+                while(i+1 < nums1.size() && nums1[i+1] == nums1[i]) i++;
+                while(j+1 < nums2.size() && nums2[j+1] == nums2[j]) j++;
+                i++; j++;
+            }
+            else if(nums1[i] < nums2[j]) i++;
+            else j++;
+        }
+        return ret;
+    }
 
-/* unorderd_hash_map
-vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-	unordered_map<int, int> element;   // store the unique nums of the shorter vector into a hash map
-	vector<int> ans;        // store the intersection of two arrays
-	for(int i = 0; i < nums2.size(); i ++){
-		// element.insert(pair<int, int>(nums2[i], 1));
-		element[nums2[i]] = 1;
-	}
-	for(int j = 0; j < nums1.size(); j ++){
-		if(element.find(nums1[j]) != element.end()){
-			ans.push_back(nums1[j]);
-			element.erase(nums1[j]);
-		}
-	}
-	return ans;
-}
-*/
-
-// unordered_set
-vector<int> intersection(vector<int>& nums1, vector<int>& nums2){
-	unordered_set<int> s(nums1.begin(), nums1.end());
-	vector<int> ans;
-	for(int i = 0; i < nums2.size(); i ++){
-		int temp = nums2[i];
-//		if(s.find(temp) != s.end()){
-		if(s.count(temp)){	// a positive count means it exits
-			ans.push_back(temp);
-			s.erase(temp);
-		}
-	}
-	return ans;
-}
-
-
-int main(){
-	// default test case
-	int case1[] = {1, 2, 2, 1};
-	int case2[] = {2, 2}; 
-	vector<int> nums1;
-	vector<int> nums2;
-	for(int i = 0; i < 4; i ++){
-		nums1.push_back(case1[i]);
-	}
-	for(int i = 0; i < 2; i ++){
-		nums2.push_back(case2[i]);
-	}
-	vector<int> ans = intersection(nums1, nums2);
-	for(int i = 0; i < ans.size(); i ++){
-		cout << ans[i] << endl;
-	}
-}
+    // hashset/hashmap: O(m + n)time, O(m or n)space, 58.27%, ok
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> ret;
+        if(nums1.empty() || nums2.empty()) return ret;
+        unordered_set<int> elem(nums1.begin(), nums1.end());
+        for(auto n: nums2){
+            if(elem.count(n)){
+                ret.push_back(n);
+                elem.erase(n);
+            }
+        }
+        return ret;
+    }
+};
