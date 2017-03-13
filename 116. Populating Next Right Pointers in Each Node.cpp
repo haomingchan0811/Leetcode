4fac350c382b69e9e77f8e06ae08f2fb0c1899a6
Haoming Chen <chenhaomingsz@gmail.com>
2017-03-13 00:54:24 -0400

    2017.3.13

--- 116. Populating Next Right Pointers in Each Node.cpp
+++ 116. Populating Next Right Pointers in Each Node.cpp
@@ -1,34 +1,43 @@
-#include <iostream>
-using namespace std;
-
-//Definition for binary tree with next pointer.
-struct TreeLinkNode {
- int val;
- TreeLinkNode *left, *right, *next;
- TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
-};
- 
+/**
+ * Definition for binary tree with next pointer.
+ * struct TreeLinkNode {
+ *  int val;
+ *  TreeLinkNode *left, *right, *next;
+ *  TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
+ * };
+ */
 class Solution {
 public:
-    // 19ms
+    // BFS: O(N)time & space, 40.61%, ok
     void connect(TreeLinkNode *root) {
-        queue<TreeLinkNode*> que;
-        if(root == NULL) return;
-        que.push(root);
-        while(true){
-            int size = que.size();
-            if(size == 0) return;
+        if(!root) return;
+        queue<TreeLinkNode*> q;
+        q.push(root);
+        while(q.size()){
+            int size = q.size();
             TreeLinkNode* prev = NULL;
-            while(size > 0){
-                TreeLinkNode* temp = que.front();
-                que.pop();
-                if(prev != NULL) prev->next = temp;
+            while(size--){
+                TreeLinkNode* temp = q.front();
+                q.pop();
+                if(prev) prev->next = temp;
                 prev = temp;
-                if(temp->left != NULL) que.push(temp->left);
-                if(temp->right != NULL) que.push(temp->right);
-                size --;
+                if(temp->left) q.push(temp->left);
+                if(temp->right) q.push(temp->right);
             }
-            prev->next = NULL;
+        }
+    }
+
+    // DFS on the left child: O(N)time, O(1)space, 40.61%, ok
+    void connect(TreeLinkNode *root) {
+        if(!root) return;
+        while(root->left){
+            TreeLinkNode* p = root;
+            while(p){
+                p->left->next = p->right;
+                if(p->next) p->right->next = p->next->left;
+                p = p->next;
+            }
+            root = root->left;
         }
     }
 };
