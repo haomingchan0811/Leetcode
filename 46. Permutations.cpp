f9455d80e8caf42f8777de09d445a01c0a07f59d
Haoming Chen <chenhaomingsz@gmail.com>
2017-03-13 12:04:56 -0400

    2017.3.13

--- 46. Permutations.cpp
+++ 46. Permutations.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    // recursive: 7.25%, ok
+    // recursive: 23.07%, ok
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ret;
         if(nums.empty()) return ret;
@@ -8,16 +8,39 @@ public:
             ret.push_back(vector<int>(1, nums[0]));
             return ret;
         }
+
         for(int i = 0; i < nums.size(); i++){
             vector<int> rest;
             for(int j = 0; j < nums.size(); j++)
                 if(j != i) rest.push_back(nums[j]);
             vector<vector<int>> remain = permute(rest);
             for(int j = 0; j < remain.size(); j++){
-                remain[j].insert(remain[j].begin(), nums[i]);
+                remain[j].push_back(nums[i]);
                 ret.push_back(remain[j]);
             }
         }
         return ret;
+    }
+
+    // recursive, without copying vector: 39.57%, ok
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ret;
+        if(nums.empty()) return ret;
+        helper(ret, nums, 0, vector<int>());
+        return ret;
+    }
+
+    void helper(vector<vector<int>>& ret, vector<int>& nums, int begin, vector<int> temp){
+        if(temp.size() == nums.size()) {
+            ret.push_back(temp);
+            return;
+        }
+        for(int i = begin; i < nums.size(); i++){
+            swap(nums[i], nums[begin]);
+            temp.push_back(nums[begin]);
+            helper(ret, nums, begin + 1, temp);
+            temp.pop_back();
+            swap(nums[i], nums[begin]);
+        }
     }
 };
