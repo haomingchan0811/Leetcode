30c17d931ab22dacca42317645a43f7ae133e237
Haoming Chen <chenhaomingsz@gmail.com>
2017-03-17 10:33:24 -0400

    2017.3.17

---
+++ 98. Validate Binary Search Tree.cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    // DFS: O(N)time & space, 57.17%, ok
+    bool isValidBST(TreeNode* root) {
+        return dfs(root, NULL, NULL);
+    }
+    
+    bool dfs(TreeNode* root, TreeNode* minNode, TreeNode* maxNode){
+        if(!root) return true;
+        if((minNode && root->val <= minNode->val) || (maxNode && root->val >= maxNode->val))
+            return false;
+        return dfs(root->left, minNode, root) && dfs(root->right, root, maxNode);
+    }
+};
