c51accfa0bb3aa6637833bddc0013d86856fb8f9
Haoming Chen <chenhaomingsz@gmail.com>
2017-07-24 00:00:10 -0700

    2017.07.23

---
+++ 128. Longest Consecutive Sequence.py
@@ -0,0 +1,48 @@
+import heapq
+class Solution(object):
+    
+    # # sort: O(NlogN) time, O(1) space
+    # def longestConsecutive(self, nums):
+    #     """
+    #     :type nums: List[int]
+    #     :rtype: int
+    #     """
+    #     if nums == []: 
+    #         return 0
+    #     ret, cnt = 0, 1
+    #     nums.sort()
+    #     for i in xrange(1, len(nums)):
+    #         if nums[i] == nums[i - 1]:
+    #             continue
+    #         elif nums[i] == nums[i - 1] + 1:
+    #             cnt += 1
+    #         else:
+    #             ret = max(ret, cnt)
+    #             cnt = 1
+    #     return max(ret, cnt)
+    
+    # # heap: O(NlogN) time, O(1) space
+    # def longestConsecutive(self, nums):
+    #     if nums == []: return 0
+    #     h = []
+    #     for num in nums:
+    #         heapq.heappush(h, num)
+    #     ret, cnt, prev = 0, 1, None
+    #     while h:
+    #         curr = heapq.heappop(h)
+    #         if prev is not None and curr == prev + 1:
+    #             cnt += 1
+    #         elif prev is not None and curr == prev:
+    #             continue
+    #         else:
+    #             ret = max(ret, cnt)
+    #             cnt = 1
+    #         prev = curr
+    #     return max(ret, cnt)
+    
+    # !! UNION FIND: O(N) time, O(N) space
+    def longestConsecutive(self, nums):
+       
+        
+        
+        
