249ae9ecdc0485408eda5aaa32503b76f721ba85
Haoming Chen <chenhaomingsz@gmail.com>
2017-08-06 00:05:13 -0700

    2017.08.05

--- 357. Count Numbers with Unique Digits.py
+++ 357. Count Numbers with Unique Digits.py
@@ -1,6 +1,6 @@
 class Solution(object):
     
-    # backtracking, dfs: O() time
+    # backtracking, dfs: O(10! + 9! + ..) time, O(10!) space
     def countNumbersWithUniqueDigits(self, n):
         """
         :type n: int
@@ -21,6 +21,16 @@ class Solution(object):
             dfs(range(10), 0, length)
             self.ret -= reduce(operator.mul, [x for x in xrange(9, 10 - length, -1)])  # leading zero
         return self.ret
+    
+    # math: O(1) time & space
+    def countNumbersWithUniqueDigits(self, n):
+        if n == 0:
+            return 1
+        ret = 10
+        for i in xrange(2, min(10, n) + 1):
+            ret += 9 * reduce(operator.mul, range(1, 10)[:10 - i - 1:-1])
+        return ret
+    
                 
             
                 
