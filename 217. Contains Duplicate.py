4fa6e5d9b438fabf31ea9a7825a050c545226740
Haoming Chen <chenhaomingsz@gmail.com>
2017-08-05 21:40:12 -0700

    2017.08.05

--- 217. Contains Duplicate.py
+++ 217. Contains Duplicate.py
@@ -1,17 +1,38 @@
-#!/usr/bin/python
-
-def containsDuplicate(nums):
-    table = {}        # dictionary to store the iterated element
-    for i in nums:
-        if (table.has_key(i)):
-            return False
-        else:
-            table[i] = 1
-           
-    return True
+class Solution(object):
     
-if __name__ == '__main__':
-    test = [1,2,3,4,5]
-    if(containsDuplicate(test)):
-        print "true"
-       
+    # brute force: O(N2) time, O(1) space, TLE
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        for i in xrange(len(nums)):
+            for j in xrange(i + 1, len(nums)):
+                if nums[i] == nums[j]:
+                    return True
+        return False
+    
+    # hash table: O(N) time & space
+    def containsDuplicate(self, nums):
+        count = {}
+        for num in nums:
+            if num in count:
+                return True
+            count[num] = 1
+        return False
+    
+    # hash set: O(N) time & space
+    def containsDuplicate(self, nums):
+        count = set(nums)
+        return len(count) != len(nums)
+    
+    # sort: O(NlogN) time, O(1) space
+    def containsDuplicate(self, nums):
+        nums.sort()
+        prev = None
+        for num in nums:
+            if prev is not None and num == prev:
+                return True
+            prev = num
+        return False
+        
