5678f1415c62a32f6ba3b7490574dad286ec5471
Haoming Chen <chenhaomingsz@gmail.com>
2017-09-05 20:29:09 -0700

    2017.09.05

--- 314. Binary Tree Vertical Order Traversal.py
+++ 314. Binary Tree Vertical Order Traversal.py
@@ -14,21 +14,18 @@ class Solution(object):
         :type root: TreeNode
         :rtype: List[List[int]]
         """
-        if root == None:
+        maps = {}
+        if root is None:
             return []
-        queue, ans = deque([(root, 0)]), dict()
-        while queue:
-            size = len(queue)
-            while size:
-                size -= 1
-                node, col = queue.popleft()
-                ans[col] = ans.get(col, []) + [node.val]
-                if node.left:
-                    queue.append((node.left, col - 1))
-                if node.right:
-                    queue.append((node.right, col + 1))
-        sorted_ans = sorted(ans.items(), key = lambda x: x[0])
-        return map(lambda x: x[1], sorted_ans)
+        queue = [(root, 0)]
+        for node, col in queue:
+            maps[col] = maps.get(col, []) + [node.val]
+            if node.left:
+                queue.append((node.left, col - 1))
+            if node.right:
+                queue.append((node.right, col + 1))
+        maps = sorted(maps.items(), key = lambda x:x[0])
+        return [x[1] for x in maps]
     
 #     # WRONG!!! order matters (If two nodes are in the same row and column, the order should be from left to right.)
 #     # dfs: O(NlogN) time & space
