45b78147500d03e1d98490cee1951fd4b1dd3424
Haoming Chen <chenhaomingsz@gmail.com>
2017-09-05 18:23:35 -0700

    2017.09.05

--- 67. Add Binary.py
+++ 67. Add Binary.py
@@ -16,6 +16,22 @@ class Solution(object):
             carry = '1' if cnt > 1 else '0'
             i, j = i-1, j-1
         return ret
+
+    def addBinary(self, a, b):
+        if a == "":
+            return b
+        if b == "":
+            return a
+        i, j, carry = len(a) - 1, len(b) - 1, 0
+        ret = ""
+        while carry or i >= 0 or j >= 0:
+            x = 1 if i >= 0 and a[i] == '1' else 0
+            y = 1 if j >= 0 and b[j] == '1' else 0
+            sums = x + y + carry
+            ret += str(sums % 2)
+            carry = sums / 2
+            i, j = i - 1, j - 1
+        return ret[::-1]
         
         
         
